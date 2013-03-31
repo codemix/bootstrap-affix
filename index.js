649fbfd94819e4849e2c1e7b050f4d63e0458607
Charles Pick <charles.pick@gmail.com>
2013-03-31 20:29:10 +0100

    Added tag v2.2.0

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function(){
   var jQuery = require('jquery');
   /* ==========================================================
-   * bootstrap-affix.js v2.1.1
+   * bootstrap-affix.js v2.2.0
    * http://twitter.github.com/bootstrap/javascript.html#affix
    * ==========================================================
    * Copyright 2012 Twitter, Inc.
@@ -30,7 +30,9 @@ function(){
   
     var Affix = function (element, options) {
       this.options = $.extend({}, $.fn.affix.defaults, options)
-      this.$window = $(window).on('scroll.affix.data-api', $.proxy(this.checkPosition, this))
+      this.$window = $(window)
+        .on('scroll.affix.data-api', $.proxy(this.checkPosition, this))
+        .on('click.affix.data-api',  $.proxy(function () { setTimeout($.proxy(this.checkPosition, this), 1) }, this))
       this.$element = $(element)
       this.checkPosition()
     }
